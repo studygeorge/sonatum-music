65a6bdb130e6115221319ae56a299b26eb80c034
root <root@cv5426645.novalocal>
2026-01-19 15:26:25 +0300

    initial import

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: 'sonatum-backend',
+      cwd: './backend',
+      script: 'npm',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3001
+      }
+    },
+    {
+      name: 'sonatum-frontend',
+      cwd: './frontend',
+      script: 'npm',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
+    }
+  ]
+};
